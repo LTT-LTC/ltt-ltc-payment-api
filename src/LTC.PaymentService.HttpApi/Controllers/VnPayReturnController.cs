adc718e8a94217c3994edb443bca373f9c05d9ab
Dranov220805 <duongthanhlong220805@gmail.com>
2026-05-13 20:27:07 +0700

    fix: fix customer booking logic

--- src/LTC.PaymentService.HttpApi/Controllers/VnPayReturnController.cs
+++ src/LTC.PaymentService.HttpApi/Controllers/VnPayReturnController.cs
@@ -35,7 +35,10 @@ public class VnPayReturnController : AbpControllerBase
             .Split('&', StringSplitOptions.RemoveEmptyEntries)
             .Select(part => part.Split('=', 2))
             .Where(parts => parts.Length == 2)
-            .ToDictionary(parts => parts[0], parts => parts[1], StringComparer.OrdinalIgnoreCase);
+            .ToDictionary(
+                parts => parts[0],
+                parts => Uri.UnescapeDataString(parts[1].Replace("+", " ")),
+                StringComparer.OrdinalIgnoreCase);
 
         var url = await _vnPayAppService.ProcessReturnAsync(dict);
         return Redirect(url);
